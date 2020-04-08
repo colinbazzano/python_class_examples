fc1ad7c404a018f626919a2fdc4f1d1606a3d63d
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-04-08 12:23:38 -0400

    goes over getter and setter

---
+++ simple-class.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+human = Person("April", "Mariano")
+
+
+print(human)
